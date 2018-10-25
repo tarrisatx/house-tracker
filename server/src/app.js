bd7e8a9522ccc5f2e458ccca32da130699309dfd
Tahara Ezell <taharaezell@webheadtech.com>
2018-10-24 21:15:13 -0500

    Backend server setup

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+//build express app now to build rest enpoints
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello tahara'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
+
+console.log('home');
